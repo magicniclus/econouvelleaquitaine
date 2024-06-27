66c53cb0d37b88010be31fc99417f53670c27e2f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-27 11:24:37 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,6 +22,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.png" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
+      <GoogleTagManager gtmId="GTM-TB8QF4NV" />
     </html>
   );
 }
